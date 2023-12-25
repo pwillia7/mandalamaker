983a84b0d2a74aed9261618ff8b41dc8f8e5731e
Patrick Williams <ptk.williams@gmail.com>
2023-12-25 16:19:02 -0600

    readme update working version

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
-WIP -- visual output doesn't match SVG output -- Needs more maths refinement for better shapes
+# Mandala Maker
 
-![](mandala.svg)
+This Python script, termed as "Mandala Maker," is designed to create and export Arab-inspired geometric patterns. These patterns are intricate and based on Islamic geometric design principles, suitable for various artistic and design applications, including laser cutting.
+
+## Features
+
+- Randomized generation of complex geometric patterns.
+- Export functionality for patterns as SVG files.
+- Adjustable parameters for pattern complexity and aesthetics.
+
+## Screenshot
+
+![Generated Mandala Pattern](screen.png)
+
+## Usage
+
+Run the script to generate an initial pattern. Interact with the 'Generate' button to create new patterns, and use the 'Save SVGs' button to export the generated patterns into SVG format for each layer.
+
+## Requirements
+
+- Python 3.x
+- Matplotlib
+- Numpy
+
+## Running the Script
+To execute the script and start the Mandala Maker:
+
+python mandala_maker.py
+
+## ToDo
+- Integrate color palette customization within the user interface.
+- Enhance the algorithm for more advanced pattern interlacing.
+- Scale up the generation for a larger number of layers while maintaining pattern clarity.
+-  Develop a graphical user interface for ease of use by those not familiar with command-line operations.
+## License
+This project is released under the MIT License.
--- mandalamaker.py
+++ mandalamaker.py
@@ -69,16 +69,109 @@ def draw_mandala(ax, center, size, num_layers):
             patch = PathPatch(path, facecolor=np.random.rand(3,), alpha=0.5, edgecolor='none')
             ax.add_patch(patch)
 
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import PathPatch
+from matplotlib.path import Path
+from matplotlib.widgets import Button
+import os
+from datetime import datetime
+import matplotlib.colors as mcolors
+import random
 
+initial_zoom_out_factor = 1.2  # This factor can be adjusted based on the pattern size
+
+def generate_color_palette():
+    # Exclude cyan by avoiding hues close to cyan's hue in the HSV color space
+    hue = random.choice(np.concatenate((np.linspace(0, 0.4, 10), np.linspace(0.6, 1, 10))))
+    # Use color harmonies such as analogous or complementary color schemes
+    complementary_hue = (hue + 0.5) % 1.0
+    analogous_hue1 = (hue + 1/12) % 1.0
+    analogous_hue2 = (hue - 1/12) % 1.0
+    colors = mcolors.hsv_to_rgb([[hue, 1, 1], [complementary_hue, 1, 1], [analogous_hue1, 1, 1], [analogous_hue2, 1, 1]])
+    return colors.tolist()
+
+
+def generate_base_shape(center, radius, num_sides):
+    angles = np.linspace(0, 2 * np.pi, num_sides, endpoint=False)
+    return [(center[0] + np.cos(angle) * radius, center[1] + np.sin(angle) * radius) for angle in angles]
+
+def generate_star(center, radius, num_points, randomness=False):
+    points = []
+    inner_radius = radius * (0.5 + random.uniform(-0.1, 0.1) if randomness else 0.5)
+    for i in range(num_points * 2):
+        angle = i * np.pi / num_points
+        r = inner_radius if i % 2 else radius
+        points.append((center[0] + np.cos(angle) * r, center[1] + np.sin(angle) * r))
+    points.append(points[0])
+    return points
+
+def create_pattern(center, size, num_layers):
+    pattern_paths = []
+    for layer in range(num_layers):
+        # Introduce more variety in the shapes
+        shape = random.choice([6, 8, 10, 12])
+        radius = size * (layer + 1) / num_layers
+        star_radius = radius * random.uniform(0.8, 1.2)
+        star_points = random.choice([6, 8, 10, 12])  # More points for complexity
+        base_shape = generate_base_shape(center, radius, shape)
+        star = generate_star(center, star_radius, star_points)
+        # Combine base shapes and stars to form a complex pattern
+        combined_pattern = base_shape + star
+        pattern_paths.append(combined_pattern)
+    return pattern_paths
+
+
+def generate_color_palette():
+    base_color = mcolors.hsv_to_rgb(np.array([random.random(), 1, 1]))
+    complementary_color = mcolors.hsv_to_rgb(np.array([(base_color[0] + 0.5) % 1.0, 1, 1]))
+    return [base_color, complementary_color]
+
+def generate_interlaced_star(center, size, num_vertices):
+    # Create the main star
+    star_points = generate_star(center, size, num_vertices)
+    # Create interlacing
+    interlaced_points = []
+    for i in range(len(star_points) - 1):
+        interlaced_points.append(star_points[i])
+        mid_point = ((star_points[i][0] + star_points[i + 1][0]) / 2,
+                     (star_points[i][1] + star_points[i + 1][1]) / 2)
+        interlaced_points.append(mid_point)
+    return interlaced_points
+
+def draw_pattern(ax, center, size, num_layers):
+    global all_paths
+    all_paths = []
+    color_palette = generate_color_palette()  # Generate a new color palette for each pattern
+
+    pattern_layers = create_pattern(center, size, num_layers)
+    base_line_width = 1.0  # Base line width for the outermost layer
+    for i, pattern in enumerate(pattern_layers):
+        # Adjust line width for visibility without overcrowding
+        line_width = max(base_line_width - 0.15 * i, 0.4)
+        color = color_palette[i % len(color_palette)]
+        pattern_path = Path(pattern + [pattern[0]])  # Close the path for each combined pattern
+        patch = PathPatch(pattern_path, facecolor='none', edgecolor=color, linewidth=line_width)
+        ax.add_patch(patch)
+        all_paths.append(pattern_path)
+
+    # Set the plot limits to ensure the pattern is centered and zoomed appropriately
+    pattern_bound = size * num_layers
+    ax.set_xlim(center[0] - pattern_bound, center[0] + pattern_bound)
+    ax.set_ylim(center[1] - pattern_bound, center[1] + pattern_bound)
+    ax.set_aspect(1)
+    ax.axis('off')
 
 def on_generate(event):
-    global all_paths
     ax.clear()
-    all_paths = []  # Clear the list of paths
-    draw_mandala(ax,(0.5, 0.5),1.0, layers)  # Adjust the size to 1.0
-    ax.relim()
-    ax.autoscale_view()
+    layers = random.randrange(5,20)
+    draw_pattern(ax, (0, 0), size, layers)
+    ax.set_xlim(-size * initial_zoom_out_factor, size * initial_zoom_out_factor)
+    ax.set_ylim(-size * initial_zoom_out_factor, size * initial_zoom_out_factor)
+    ax.set_aspect(1)
+    ax.axis('off')
     fig.canvas.draw()
+
 
 
 
@@ -102,17 +195,19 @@ def on_save(event):
         save_svg(path, filename)
 
 size = 1.0
-layers = 5
+layers = random.randrange(5,20)
 all_paths = []
 
 fig, ax = plt.subplots()
-btn_generate = Button(plt.axes([0.7, 0.05, 0.1, 0.075]), 'Generate')
+button_width = 0.15
+button_height = 0.075
+btn_generate = Button(plt.axes([0.7, 0.05, button_width, button_height]), 'Generate')
 btn_generate.on_clicked(on_generate)
-btn_save = Button(plt.axes([0.81, 0.05, 0.1, 0.075]), 'Save SVGs')
+btn_save = Button(plt.axes([0.7 + button_width + 0.01, 0.05, button_width, button_height]), 'Save SVGs')
 btn_save.on_clicked(on_save)
 
-ax.set_xlim(-0.5, 0.5)
-ax.set_ylim(-0.5, 0.5)
+ax.set_xlim(-size * initial_zoom_out_factor, size * initial_zoom_out_factor)
+ax.set_ylim(-size * initial_zoom_out_factor, size * initial_zoom_out_factor)
 
 ax.set_aspect(1)
 ax.axis('off')
